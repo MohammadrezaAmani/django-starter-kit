381377ee3e1af8821afc5cfe03e055c4cc38f1dc
Mohammadreza Amani <More.Amani@yahoo.com>
2025-05-16 02:50:52 +0330

    Add IsAdminUserOrReadOnly permission class

---
+++ accounts/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+
+class IsAdminUserOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow admin users to create, update, and delete objects.
+    Read-only access is allowed for other users.
+    """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
