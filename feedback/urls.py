87d54190211192b0e20d9f77f6d2e17dac356912
Mohammadreza Amani <More.Amani@yahoo.com>
2025-05-16 15:32:57 +0330

    Simplify feedback URLs
    Use router.urls directly and remove the "feedback" prefix
    from the endpoint.

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,11 +1,8 @@
-from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import FeedbackViewSet
 
 router = DefaultRouter()
-router.register(r"feedback", FeedbackViewSet, basename="feedback")
+router.register("", FeedbackViewSet, basename="feedback")
 
-urlpatterns = [
-    path("api/", include(router.urls)),
-]
+urlpatterns = router.urls
