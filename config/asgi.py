17304c4ec6cf76bc859d17108d50c4df01a9a482
Mohammadreza Amani <More.Amani@yahoo.com>
2025-05-16 04:13:04 +0330

    Configure ASGI for Channels

--- config/asgi.py
+++ config/asgi.py
@@ -1,7 +1,21 @@
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+from notifications.urls import (
+    websocket_urlpatterns as notifications_websocket_urlpatterns,
+)
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(notifications_websocket_urlpatterns))
+        ),
+    }
+)
