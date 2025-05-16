5b1e2a41565c3edde85bdcc9ff15e695e0555cc0
Mohammadreza Amani <More.Amani@yahoo.com>
2025-05-16 10:04:27 +0330

    Add common urls
    Define API endpoints for common models

---
+++ common/urls.py
@@ -0,0 +1,23 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    ActionViewSet,
+    CommentViewSet,
+    LocationViewSet,
+    ReactViewSet,
+    TagViewSet,
+    ViewViewSet,
+)
+
+router = DefaultRouter()
+router.register(r"tags", TagViewSet, basename="tag")
+router.register(r"actions", ActionViewSet, basename="action")
+router.register(r"reacts", ReactViewSet, basename="react")
+router.register(r"views", ViewViewSet, basename="view")
+router.register(r"comments", CommentViewSet, basename="comment")
+router.register(r"locations", LocationViewSet, basename="location")
+
+urlpatterns = [
+    path("api/", include(router.urls)),
+]
