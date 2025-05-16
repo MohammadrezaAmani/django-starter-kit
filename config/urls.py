af236f3a6e89a333b82682e86a0a2990872c051c
Mohammadreza Amani <More.Amani@yahoo.com>
2025-05-16 04:13:13 +0330

    Add notifications app to urls

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path("notifications/", include("notifications.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/docs/",
